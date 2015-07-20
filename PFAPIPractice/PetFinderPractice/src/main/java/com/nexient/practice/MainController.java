d2026309721353cf93e9bf75b91bd1d2eb9c5958
mfender <mfender@LT-5CG4521KQF.c.systemsinmotion.com>
2015-07-20 11:59:32 -0400

    Update greeting to use user's TimeZone rather than server's

--- PFAPIPractice/PetFinderPractice/src/main/java/com/nexient/practice/MainController.java
+++ PFAPIPractice/PetFinderPractice/src/main/java/com/nexient/practice/MainController.java
@@ -1,8 +1,12 @@
 package com.nexient.practice;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Controller // IDs class as a MVC controller
 public class MainController {
@@ -15,7 +19,13 @@ public class MainController {
 	 *         frontpage.html)
 	 */
 	public String greeting(final Model model) {
-		model.addAttribute("timeOfDay", TimeOfDay.greeting());
+		// Get user's TimeZone
+		ZoneId tz = TimeZone
+			.getTimeZone(
+				SessionLocaleResolver.TIME_ZONE_SESSION_ATTRIBUTE_NAME)
+			.toZoneId();
+		// Greet with user's time of day
+		model.addAttribute("timeOfDay", TimeOfDay.greeting(tz));
 		return "frontpage";
 	}
 
--- PFAPIPractice/PetFinderPractice/src/main/java/com/nexient/practice/TimeOfDay.java
+++ PFAPIPractice/PetFinderPractice/src/main/java/com/nexient/practice/TimeOfDay.java
@@ -1,16 +1,20 @@
 package com.nexient.practice;
 
-import java.time.LocalTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 public final class TimeOfDay {
-	private final static LocalTime	EVENING	= LocalTime.of(17, 0);
-	private final static LocalTime	NOON		= LocalTime.of(12, 0);
 
-	public static String greeting() {
+	public static String greeting(final ZoneId tz) {
+		final ZoneOffset zo = (ZoneOffset) tz.normalized();
+		final OffsetTime EVENING = OffsetTime.of(17, 0, 0, 0, zo);
+		final OffsetTime NOON = OffsetTime.of(12, 0, 0, 0, zo);
+		final OffsetTime ot = OffsetTime.now(zo);
 		String timeOfDay;
-		if (LocalTime.now().isBefore(TimeOfDay.NOON)) {
+		if (ot.isBefore(NOON)) {
 			timeOfDay = "morning";
-		} else if (LocalTime.now().isBefore(TimeOfDay.EVENING)) {
+		} else if (ot.isBefore(EVENING)) {
 			timeOfDay = "afternoon";
 		} else {
 			timeOfDay = "evening";
